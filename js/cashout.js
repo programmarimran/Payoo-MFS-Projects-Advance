0c6566b734e128e68ba3420ef1c1e82f1f14c0c2
programmarimran <programmarimran@gmail.com>
2025-02-27 00:52:57 +0600

    added a cashout section is done

--- js/cashout.js
+++ js/cashout.js
@@ -5,12 +5,27 @@ document.getElementById("btn-cashout")
     const cashoutAmount=document.getElementById("cashout-amount").value;
     const transferCashoutAmount=parseFloat(cashoutAmount);
     const cashoutPin=document.getElementById("cashout-pin").value;
-    const transferCashoutPin=parseIn(cashoutPin);
-    const mainBalance=getElementById("main-balance").innerText;
-    const transferMainBalance=parseFloat(mainBalance);
+    const transferCashoutPin=parseInt(cashoutPin);
+    const cashoutMainBalance =document.getElementById("main-balance").innerText;
+    const transferCashoutmainBalance=parseFloat(cashoutMainBalance);
     if(agentNumber.length!==11){
         alert("please valid agent number")
     }
-    
+    else if(transferCashoutAmount<100){
+      alert("  please minimum cashout amount 100 tk")
+    }
+    else if(transferCashoutPin!==1234){
+        alert("please valid pin ")
+    }
+    else{
+        if(transferCashoutmainBalance<=0 ==true){
+            document.getElementById("main-balance").innerText=0;
+            
+        }
+        else{
+            const substraction =transferCashoutmainBalance-transferCashoutAmount;
+        document.getElementById("main-balance").innerText=substraction;
+        }
+    }
 
 })
